de202fd84104e9bdd513f0a39f281bfd9a9d759e
Robert Lasch <robert.lasch0@googlemail.com>
2019-12-05 22:43:25 +0100

    Improve the Intcode function

--- day2/main.rs
+++ day2/main.rs
@@ -8,27 +8,27 @@ fn main() {
     let mut tape = Vec::new();
     for opcode in input.split(b',') {
         let op_str = String::from_utf8(opcode.unwrap()).unwrap();
-        let op: usize = op_str.trim_end().parse().unwrap();
+        let op: i64 = op_str.trim_end().parse().unwrap();
         tape.push(op);
     }
 
     tape[1] = 12;
     tape[2] = 2;
 
-    println!("Part 1: {}", compute_output(&tape));
+    println!("Part 1: {}", compute_output(&tape)[0]);
 
     for noun in 0..99 {
         for verb in 0..99 {       
             tape[1] = noun;
             tape[2] = verb;
-            if compute_output(&tape) == 19690720 {
+            if compute_output(&tape)[0] == 19690720 {
                 println!("Part 2: {}", 100 * noun + verb);
             }
         }
     }
 }
 
-fn compute_output(memory: &[usize]) -> usize {
+fn compute_output(memory: &[i64]) -> Vec<i64> {
     let mut tape = memory.to_vec();
 
     let mut address = 0;
@@ -37,16 +37,16 @@ fn compute_output(memory: &[usize]) -> usize {
         if opcode == 99 {
             break;
         } else if opcode == 1 {
-            let param1_address = tape[address + 1];
-            let param2_address = tape[address + 2];
-            let out_address = tape[address + 3];
+            let param1_address = tape[address + 1] as usize;
+            let param2_address = tape[address + 2] as usize;
+            let out_address = tape[address + 3] as usize;
             let param1 = tape[param1_address];
             let param2 = tape[param2_address];
             tape[out_address] = param1 + param2;
         } else if opcode == 2 {
-            let param1_address = tape[address + 1];
-            let param2_address = tape[address + 2];
-            let out_address = tape[address + 3];
+            let param1_address = tape[address + 1] as usize;
+            let param2_address = tape[address + 2] as usize;
+            let out_address = tape[address + 3] as usize;
             let param1 = tape[param1_address];
             let param2 = tape[param2_address];
             tape[out_address] = param1 * param2;
@@ -56,5 +56,5 @@ fn compute_output(memory: &[usize]) -> usize {
         address += 4;
     }
 
-    tape[0]
+    tape
 }
