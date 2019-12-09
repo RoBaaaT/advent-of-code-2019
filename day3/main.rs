28b9da274c47281c26df685498b815ee328aeed4
Robert Lasch <robert.lasch0@googlemail.com>
2019-12-09 22:13:45 +0100

    Solution day 3 part 2

--- day3/main.rs
+++ day3/main.rs
@@ -10,7 +10,8 @@ fn main() {
     const MAP_SIZE: usize = 8192 * SCALE;
     const ORIGIN: usize = 4096 * SCALE;
     for line in input.lines() {
-        let mut map = vec![false; MAP_SIZE * MAP_SIZE];
+        let mut map = vec![0; MAP_SIZE * MAP_SIZE];
+        let mut steps = 1;
         let mut x = ORIGIN;
         let mut y = ORIGIN;
         for entry in line.unwrap().split(",") {
@@ -18,20 +19,32 @@ fn main() {
             let distance: usize = entry[1..entry.len()].parse().unwrap();
             match direction {
                 "L" => {
-                    for offset in 0..distance { map[y * MAP_SIZE + x - 1 - offset] = true; }
-                    x -= distance
+                    for offset in 0..distance {
+                        if map[y * MAP_SIZE + x - 1 - offset] == 0 { map[y * MAP_SIZE + x - 1 - offset] = steps; }
+                        steps += 1;
+                    }
+                    x -= distance;
                 },
                 "R" => {
-                    for offset in 0..distance { map[y * MAP_SIZE + x + 1 + offset] = true; }
-                    x += distance
+                    for offset in 0..distance {
+                        if map[y * MAP_SIZE + x + 1 + offset] == 0 { map[y * MAP_SIZE + x + 1 + offset] = steps; }
+                        steps += 1;
+                    }
+                    x += distance;
                 },
                 "D" => {
-                    for offset in 0..distance { map[(y - 1 - offset) * MAP_SIZE + x] = true; }
-                    y -= distance
+                    for offset in 0..distance {
+                        if map[(y - 1 - offset) * MAP_SIZE + x] == 0 { map[(y - 1 - offset) * MAP_SIZE + x] = steps; }
+                        steps += 1;
+                    }
+                    y -= distance;
                 },
                 "U" => {
-                    for offset in 0..distance { map[(y + 1 + offset) * MAP_SIZE + x] = true; }
-                    y += distance
+                    for offset in 0..distance {
+                        if map[(y + 1 + offset) * MAP_SIZE + x] == 0 { map[(y + 1 + offset) * MAP_SIZE + x] = steps; }
+                        steps += 1;
+                    }
+                    y += distance;
                 },
                 _ => panic!("invalid direction code: {}", direction)
             }
@@ -39,16 +52,22 @@ fn main() {
         maps.push(map);
     }
     let mut min_distance = std::usize::MAX;
+    let mut min_steps = std::usize::MAX;
     for x in 0..MAP_SIZE {
         for y in 0..MAP_SIZE {
-            if maps[0][y * MAP_SIZE + x] == true && maps[1][y * MAP_SIZE + x] == true {
+            if maps[0][y * MAP_SIZE + x] != 0 && maps[1][y * MAP_SIZE + x] != 0 {
                 let distance = if x > ORIGIN { x - ORIGIN } else { ORIGIN - x }
                     + if y > ORIGIN { y - ORIGIN } else { ORIGIN - y };
+                let steps = maps[0][y * MAP_SIZE + x] + maps[1][y * MAP_SIZE + x];
                 if distance != 0 && distance < min_distance {
                     min_distance = distance;
+                }
+                if steps != 0 && steps < min_steps {
+                    min_steps = steps;
                 }
             }
         }
     }
     println!("Part 1: {}", min_distance);
+    println!("Part 2: {}", min_steps);
 }
