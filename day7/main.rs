216ce325731c7e3bcfad85b330e62fc40848790d
Robert Lasch <robert.lasch0@googlemail.com>
2019-12-14 17:50:46 +0100

    Solution day 7 part 2

--- day7/main.rs
+++ day7/main.rs
@@ -1,16 +1,82 @@
 use std::fs::File;
 use std::ops::Range;
+use std::sync::{Arc, RwLock};
+use std::collections::VecDeque;
+use std::thread;
 use intcode::*;
 
-fn amp(memory: &Vec<i64>, phase_settings: &Vec<i64>) -> i64 {
-    let mut memory = memory.clone();
-    let mut prev_output = 0;
-    for phase in phase_settings {
-        let mut output = VecOutput::new();
-        memory = execute_intcode(&memory, &mut VecInput::new(vec![phase.clone(), prev_output]), &mut output);
-        prev_output = output.values()[0];
+struct IOLink {
+    buffer: Arc<RwLock<VecDeque<i64>>>
+}
+
+impl IOLink {
+    fn new() -> IOLink {
+        IOLink { buffer: Arc::new(RwLock::new(VecDeque::new())) }
     }
-    prev_output
+
+    fn from_slice(values: &[i64]) -> IOLink {
+        let result = Self::new();
+        for value in values { result.buffer.write().unwrap().push_back(*value); }
+        result
+    }
+
+    fn output(&self) -> IOLinkOutput {
+        IOLinkOutput { buffer: self.buffer.clone() }
+    }
+
+    fn input(&self) -> IOLinkInput {
+        IOLinkInput { buffer: self.buffer.clone() }
+    }
+}
+
+struct IOLinkOutput {
+    buffer: Arc<RwLock<VecDeque<i64>>>
+}
+
+struct IOLinkInput {
+    buffer: Arc<RwLock<VecDeque<i64>>>
+}
+
+impl Output for IOLinkOutput {
+    fn output(&mut self, value: i64) {
+        self.buffer.write().unwrap().push_back(value);
+    }
+}
+
+impl Input for IOLinkInput {
+    fn get_next(&mut self) -> i64 {
+        loop {
+            if let Some(value) = self.buffer.write().unwrap().pop_front() {
+                break value;
+            }
+        }
+    }
+}
+
+
+fn amp(memory: &Vec<i64>, phase_settings: &Vec<i64>) -> i64 {
+    let mut links = Vec::new();
+    for (i, phase) in phase_settings.iter().enumerate() {
+        links.push(IOLink::from_slice(&[*phase]));
+        if i == 0 {
+            links[0].buffer.write().unwrap().push_back(0);
+        }
+    }
+    let mut threads = Vec::new();
+    for i in 0..links.len() {
+        let next_link = if i == links.len() - 1 { 0 } else { i + 1 };
+        let thread_memory = memory.clone();
+        let mut input = links[i].input();
+        let mut output = links[next_link].output();
+        threads.push(thread::spawn(move || {
+            execute_intcode(&thread_memory, &mut input, &mut output);
+        }));
+    }
+    for thread in threads {
+        thread.join().unwrap();
+    }
+    let result: i64 = *links[0].buffer.read().unwrap().back().unwrap();
+    result
 }
 
 fn find_highest_signal(memory: &Vec<i64>, phase_range: Range<i64>) -> i64 {
@@ -54,4 +120,5 @@ fn main() {
     let input_file = File::open("input.txt").unwrap();
     let memory = load_tape(input_file);
     println!("Part 1: {}", find_highest_signal(&memory, 0..5));
+    println!("Part 2: {}", find_highest_signal(&memory, 5..10));
 }
